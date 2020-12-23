ad9c47b49c0efee854c7af26115957ddc8bbb28c
Hans Martin <hansmrtn@gmail.com>
2020-12-22 23:12:08 -0800

    updated c to utlizie struct for unified program'

--- ncgrid.c
+++ ncgrid.c
@@ -4,8 +4,8 @@
 
 #define NCFILE "wrfout.nc"
 
-int main() 
-{
+int main() {
+
 	void check(int status) 
 	{
 		if ( status != NC_NOERR ) {
@@ -13,21 +13,22 @@ int main()
 		}
 	}
 
-	int status; 
+	int status; // io for fortran
 
 	int ncid;
 	int latid, lonid;
 	char latname, lonname;
 	size_t latlen, lonlen;
 
-	
+	// Open the nc file	
 	check(nc_open(NCFILE, NC_NOWRITE, &ncid));
 
+	// get lon lat ids
 	check(nc_inq_dimid(ncid, "west_east", &lonid));
 	check(nc_inq_dimid(ncid, "south_north", &latid));
 
+	// get dims (nx ny) of lat lon dimensions
 	check(nc_inq_dimlen(ncid, lonid, &lonlen));
 	check(nc_inq_dimlen(ncid, latid, &latlen));
-
 
 }
--- unpack.c
+++ unpack.c
@@ -1,44 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
-void main() {
-
-	FILE *arl;
+struct Arl {
 
 	char header_label[50]; 
-	char header[3072]; 
+	char header[158];
 
 	int year, month, day, hour, ifc;
-
 	char vdesc[4];
 
+	char model[4];
+	int grid_num; 
+	int z_coord;
 	float pole_lat, pole_lon, ref_lat, ref_lon, grid_size, orient; 
 	float tan_lat, sync_x, sync_y, sync_lat, sync_lon, reserved; 
 	int nx, ny, nz; 
+	int kflg, lenh;
 
+}; 
 
-	arl = fopen("wrf.arl", "r");
-	//int arl;
+void main() {
 
-	//header_label = fseek(arl, 158L, SEEK_SET);
+	struct Arl arl;
 
-	fgets(header, 158, arl);
+	FILE * arl_file; // arl file
 
-	//printf("%s", header_label);
+	// open the file
+	arl_file = fopen("wrf.arl", "rb");
 
-	sscanf(header, "%2d %2d %2d %2d %2d %*d %4s", 
-		&year, &month, &day, &hour, &ifc, vdesc);
+	printf("Header Label: \n");
+	//fseek(arl_file, 0, SEEK_SET);
+	fread(&arl, sizeof(arl.header_label), 1, arl_file);
+	sscanf(arl.header_label, "%2d%2d%2d%2d%2d%*d%4s", 
+		&arl.year, &arl.month, &arl.day, &arl.hour, &arl.ifc, arl.vdesc);
+	printf("%s\n", arl.header_label);
 
+	printf("Header: \n");
+	fseek(arl_file, 0, SEEK_SET);
+	fread(&arl, sizeof(arl.header), 1, arl_file);
+	sscanf(arl.header, "%4s%3d%1d%f%f%f%f%f%f%f%f%f%f%f%f%3d%3d%3d", 
+		arl.model,      &arl.grid_num,  &arl.z_coord, 
+		&arl.pole_lat,  &arl.pole_lon,  &arl.ref_lat, 
+		&arl.ref_lon,   &arl.grid_size, &arl.orient,   
+		&arl.tan_lat,   &arl.sync_x,    &arl.sync_y,    
+		&arl.sync_lat,  &arl.sync_lon,  &arl.reserved, 
+		&arl.nx, 		&arl.ny, 		&arl.nz);
 
-
-
-
-
- 	printf("%s\n", header);
-	//printf("%d, %d, %d, %d\n", year, month, day, hour);	
-	//sscanf() 
+	printf("%d\n", arl.nx);
 	
-
+	fclose(arl_file);
 
 
 }
