674b9ae9a2f76c2b26912ee77cc7c29e81501eb8
Hans Martin <hansmrtn@gmail.com>
2020-12-26 00:38:27 -0800

    finally resolved header decoding of proper length

--- unpack.c
+++ unpack.c
@@ -2,22 +2,33 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define A 8
+
 struct header {
-    char src[4]; char icx[3]; char mn[2];
-    char polelat[7]; char polelon[7]; char reflat[7]; 
-    char reflon[7]; char size[7]; char orient[7]; 
-    char tanlat[7]; char syncxp[7]; char syncyp[7]; 
-    char synclat[7]; char synclon[7]; char res[7]; 
-    char nx[3]; char ny[3]; char nz[3];
-    char kflag[2]; char lenh[4];
+    char src[A];        char icx[A];        char mn[A];
+    char polelat[A];    char polelon[A];    char reflat[A]; 
+    char reflon[A];     char size[A];       char orient[A]; 
+    char tanlat[A];     char syncxp[A];     char syncyp[A]; 
+    char synclat[A];    char synclon[A];    char res[A]; 
+    char nx[A];         char ny[A];         char nz[A];
+    char kflag[A];      char lenh[A];
 };
 
-struct header hdecode(struct header h, char *str)
+struct label {
+
+};
+
+void hdecode(struct header *h, char *str)
 {
-
-    memcpy(&h, str, 108);
-    return h;
-
+    sscanf(str, 
+    "%4c%3c%2c%7c%7c%7c%7c%7c%7c%7c%7c%7c%7c%7c%7c%3c%3c%3c%2c%4c", 
+        h->src,     h->icx,     h->mn,
+        h->polelat, h->polelon, h->reflat, 
+        h->reflon,  h->size,    h->orient, 
+        h->tanlat,  h->syncxp,  h->syncyp, 
+        h->synclat, h->synclon, h->res, 
+        h->nx,      h->ny,      h->nz, 
+        h->kflag,   h->lenh);
 } 
 
 int main() {
@@ -28,19 +39,19 @@ int main() {
 
     char label[50];
     char header[3072];
-    size_t lenh;
 
-    struct header head;
+    struct header *head = (struct header *)malloc(sizeof(struct header));
 
     // Read the standard portion of the label(50) and header(108),
     // decode the header to get dims for recl.
     fread(label, sizeof(char), 50, fparl); 
     fread(header, sizeof(char), 108, fparl);
 
-    head = hdecode(head, header);
+    hdecode(head, header);
 
-    //printf("%s\n%d\n%d\n%f\n%f\n%f",head.src, head.icx, head.mn, head.polelat, head.polelon, head.reflat);
-    //printf("%s\n%s\n", head.src, head.nx);
+    puts(head->syncyp);
+
+    
     fclose(fparl);
 
     // Read 
