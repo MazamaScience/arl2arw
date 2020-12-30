5960ef8c76f5930e6190449cae2cfee521180512
Hans Martin <hansmrtn@gmail.com>
2020-12-29 22:12:44 -0800

    a good place

--- unpack.c
+++ unpack.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
+#include <limits.h>
 
 int numX (char *str);
 int numY (char *str);
@@ -9,14 +10,16 @@ char *varDesc (char *str);
 long numRecs (long size, long recl);
 double numExp (char *str);
 double numVar1 (char *str);
-float unpack (char *cdata, double *rdata, int nx, int ny, double nexp, double var1);
+//float unpack1 (char *cdata, double *rdata, int nx, int ny, double nexp, double var1);
+void unpack2(double nexp, double var1, size_t nx, size_t ny, char cdata[], double rdata[(size_t) nx][(size_t) ny]);
+void arr_print(size_t x, size_t y, double arr[y][x]);
 
 int main ( int argc, char **argv )
 {
   FILE *arl;			  // ARL file stream 
   char fpath[80];
   char *cdata;			// packed character 
-  double *rdata;		// unpacked real  
+  //double *rdata;		// unpacked real  
 
   char *label;			// standard label 50-bytes
   char *header;			// header 108-bytes
@@ -55,12 +58,17 @@ int main ( int argc, char **argv )
   recl = nxy + 50;
 
   // Allocate array space
-  cdata = (char *) malloc (sizeof (char) * nxy);
-  rdata = (double *) malloc (sizeof (double) * nx * ny);
+  cdata = malloc (sizeof (char) * nxy);
+
+  // Create 2d array pointer
+  //rdata = (double *) malloc (sizeof (double) * nx * ny); old 
+  double (*rdata)[(size_t) nx][(size_t) ny];
+
+  rdata = malloc (sizeof (double[(size_t) nx][(size_t) ny]));
 
   rewind (arl);
   rec = 0;
-  while (rec < 2)//numRecs (fsize, recl)) //  DEBUG
+  while (rec <numRecs (fsize, recl))
   {
     ++rec;
     fread (label, sizeof (char), 50, arl);
@@ -69,7 +77,11 @@ int main ( int argc, char **argv )
     {
       nexp = numExp (label);
       var1 = numVar1 (label);
-      unpack (cdata, rdata, nx, ny, nexp, var1);
+      printf("\n%s\n", label);
+      //unpack (cdata, rdata, nx, ny, nexp, var1); 
+
+      unpack2(nexp, var1, (size_t) nx, (size_t) ny, cdata, *rdata);
+
     }
   }
 
@@ -130,41 +142,81 @@ double numExp (char *str)
 
 double numVar1 (char *str)
 {
-  char var1[8];			// +1
-  int var1pos = 22;
+  char var1[18];			// +1
+  int var1pos = 36;
   double var;
-  strncpy (var1, str + var1pos, 7);
+  strncpy (var1, str + var1pos, 17);
+  var1[17] = '\0';
   var = atof (var1);
   return var;
 }
 
 // needs work
-float unpack (char *cdata, double *rdata, int nx, int ny, double nexp, double var1)
+// float unpack1 (char *cdata, double *rdata, int nx, int ny, double nexp, double var1)
+// {
+//   double scale, vold, test;
+//   int idx, i, j;
+//   scale = pow (2.0, 7.0 - nexp);
+//   vold = var1;			// Save prev value
+//   idx = 0;
+
+//   for (j = 0; j < ny; ++j)
+//   {
+//     for (i = 0; i < nx; ++i)
+//     {
+//       if (j > 1) 
+//       {
+//         //get var 1
+//         exit(0); // debug
+//       } 
+//       test = ((unsigned) cdata[idx] ) / scale + vold;
+//       vold = *(rdata + j * ny + i);
+//       printf ("%lf\n", test);
+//       ++idx;
+//       // Whhat about edge cases like index 1 or end of??
+
+//     }
+//     vold = *(rdata + j * ny);
+//   }
+
+//   return 0;
+// }
+
+void unpack2 (double nexp, double var1, size_t nx, size_t ny, char cdata[], double rdata[(size_t) nx][(size_t) ny])
 {
-  double scale, vold;
-  int idx, i, j;
-  scale = pow (2.0, 7.0 - nexp);
-  vold = var1;			// Save prev value
-  idx = 0;
+  int indx = 0; 
+  double vold = var1; 
+  double scale; 
 
-  for (j = 1; j < ny; ++j)
+  scale = pow(2.0, (7.0 - nexp));
+
+  for (size_t j = 0; j < ny; ++j)
   {
-    for (i = 1; i < nx; ++i)
+    for (size_t i = 0; i < nx; ++i)
     {
-      if (i < 1 && j < 1) 
+      rdata[i][j] = (cdata[indx] - 127.0) / scale + vold;
+      vold = rdata[i][j];
+      ++indx; 
+
+      if (i < 2 && j < 2)
       {
-        //get var 1
+        printf ("\t%lf\n", vold);
       } 
-      printf (": %lf", vold);
-      *(rdata + j * ny + i) = ((int) cdata[idx] - 127.0) / scale + vold;
-      vold = *(rdata + j * nx + i);
-      printf("\n INDEX: %d\n",idx);
-      ++idx;
-      // Whhat about edge cases like index 1 or end of??
-
+      
     }
-    vold = *(rdata + j * nx);
+    vold = rdata[0][j]; 
   }
+  //arr_print(nx, ny, rdata);
+}
 
-  return 0;
+void arr_print (size_t x, size_t y, double arr[x][y])
+{
+    for(size_t j=0; j<y; j++)
+  {
+    for(size_t i=0; i<x; i++)
+    {
+      printf("\n(%ld, %ld): %lf\t", j, i, arr[i][j]);
+    }
+    printf("\n");
+  }
 }
